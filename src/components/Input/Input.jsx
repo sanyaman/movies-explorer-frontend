26e65940cec5a955456744dec0f273dfb18ca695
sanyaman <sanyaman@gmail.com>
2023-08-29 01:32:45 +0300

    rev2

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -5,7 +5,7 @@ function Input({title, id, name, type}) {
     return (
         <>
             <p className='input__title'>{title}</p>
-            <input className='input' required  id={id} name={name} type={type}/>
+            <input className='input' required={true}  id={id} name={name} type={type}/>
             <span className='input__error'>Что-то пошло не так...</span>
         </>
     )
